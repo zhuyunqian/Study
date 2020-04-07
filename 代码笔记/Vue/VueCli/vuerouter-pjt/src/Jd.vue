1e5d41ca6e31bbdd6ebf93ba741880792eb76a22
zhutongxue <qtzhuyunqian@163.com>
2020-04-08 00:24:55 +0800

    Vue-router

---
+++ 代码笔记/Vue/VueCli/vuerouter-pjt/src/Jd.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      京东首页
+      <router-view></router-view>
+      <footer>
+          <ul>
+              <li>首页</li>
+              <li>分类</li>
+              <li>购物车</li>
+              <li>我的</li>
+          </ul>
+      </footer>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ 代码笔记/Vue/VueCli/vuerouter-pjt/src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <p>a:{{a}}  b:{{b}}</p>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      a:'',
+      b:''
+    }
+  },
+  components: {
+    HelloWorld
+  },
+  created() {
+    console.log(this.$router);
+    const query = this.$router.query;
+    this.a = query.a;
+    this.b = query.b;
+  },
+}
+</script>
