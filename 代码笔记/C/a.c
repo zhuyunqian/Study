d10f74262d32cc0ee95971ee5ba9bc31219b0664
zhutongxue <qtzhuyunqian@163.com>
2020-04-08 00:37:33 +0800

    C语言 -- 计算机 -- 基础使用

---
+++ 代码笔记/C/a.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int a1 =22;
+    int b1 = 33;
+    int maxNum = max(a1,b1);
+    int minNum = min(a1,b1);
+    printf("that maxnumber is %d that minbunber is %d",maxNum,minNum);
+    return 0;
+}
