51b14ec44a477f9d1b821dfa84304bf7fb9eb7d4
zhutongxue <qtzhuyunqian@163.com>
2020-04-08 11:36:13 +0800

    Element-ui  -- home -- menu --   点击获取路由

--- 代码笔记/Vue/VueCli/elementui-pjt/src/views/homepage/children/menu.vue
+++ 代码笔记/Vue/VueCli/elementui-pjt/src/views/homepage/children/menu.vue
@@ -1,22 +1,20 @@
 <template>
   <div>
       <section class="home-menu">
-              <el-menu
-                default-active="2"
-                class="el-menu-vertical-demo"
-                @open="handleOpen"
-                @close="handleClose"
-                background-color="#545c64"
-                text-color="#fff"
-                active-text-color="#ffd04b">
-               <el-submenu index="1" v-for="(menu ,index) in menuConfig " :key="index" >
+        <el-menu
+        :default-active="activeRoute"
+        class="el-menu-vertical-demo"
+        background-color="#545c64"
+        text-color="#fff"
+        active-text-color="#ffd04b">
+            <el-submenu class="bgtitle" index="1" v-for="(menu ,index) in menuConfig " :key="index" >
                 <template slot="title">
                 <i class="el-icon-location"></i>
                 <span>{{menu.title}}</span>
                 </template>
                 <el-menu-item v-for="(childenmen,childindex) in menu.subs" :key="childindex">{{childenmen.title}}</el-menu-item>
-            </el-submenu>
-                </el-menu>
+                </el-submenu>
+        </el-menu>
   
       </section>
   </div>
@@ -31,11 +29,12 @@ export default {
         }
     },
     methods: {
-        handleOpen(key, keyPath) {
-            console.log(key, keyPath);
-        },
-        handleClose(key, keyPath) {
-            console.log(key, keyPath);
+        
+    },
+    computed: {
+        activeRoute(){
+            console.log(this.$route.path);
+            return this.$route.path
         }
     },
     
@@ -43,12 +42,18 @@ export default {
 </script>
 
 <style lang="less" scoped>
+    @bgcolor :#324157;
     .home-menu{
         position: absolute;
         top:70px;
         left:0;
         bottom: 0;
         width:250px;
+        background:@bgcolor;
+
+        .bgtitle{
+            background:@bgcolor;
+        }
         
     }
 </style>
