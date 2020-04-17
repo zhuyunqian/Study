bd70e404a81a628c3206237576238190c2c8f377
zhutongxue <qtzhuyunqian@163.com>
2020-04-17 19:05:08 +0800

    Vue -- VueRouter -- 京东案例  -- 介绍main.js

--- 代码笔记/Vue/VueCli/vuerouter-pjt/src/main.js
+++ 代码笔记/Vue/VueCli/vuerouter-pjt/src/main.js
@@ -8,7 +8,10 @@ import store from './store'
 Vue.config.productionTip = false
 
 new Vue({
+  //这里的router和store是引入的router里面的js然后把模块导出的
+  //这里相当于把他挂载到实力化上面了
   router,
   store,
   render: h => h(Jd)
 }).$mount('#app')
+//替换挂载的节点 -- 这里的#app -- public-indexhtml上的#app节点
