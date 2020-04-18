c1021b25d106611a12eea0d3e3bcfdec6bc90dd1
zhutongxue <qtzhuyunqian@163.com>
2020-04-18 18:07:57 +0800

    --  整理路径  --

--- 代码笔记/Vue/VueCli/elementui-pjt/src/plugins/element.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue'
-import { Button,Form,FormItem,Input,Message,Dropdown,DropdownItem,DropdownMenu,MenuItem,Menu,Submenu,MenuItemGroup } from 'element-ui'
-
-Vue.use(Button).use(Form).use(FormItem).use(Input).use(Dropdown).use(DropdownItem).use(DropdownMenu).use(MenuItem).use(Menu).use(Submenu).use(MenuItemGroup)
-Vue.prototype.$message = Message
--- 代码笔记/Vue/VueCli/elementui-pjt/vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    devServer:{
-        proxy:{
-            '/tokens':
-            {target:'http://kumanxuan1.f3322.net:8084'}
-        }
-    }
-}   
