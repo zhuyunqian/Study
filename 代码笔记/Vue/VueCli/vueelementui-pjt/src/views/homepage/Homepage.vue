daea5899a2674101a1c365c594cbfac2af4c6022
zhutongxue <qtzhuyunqian@163.com>
2020-04-18 16:12:59 +0800

    Vue -- elementUi -- 后台管理项目 -- home页面 -- header(添加组件及应用)

--- 代码笔记/Vue/VueCli/vueelementui-pjt/src/views/homepage/Homepage.vue
+++ 代码笔记/Vue/VueCli/vueelementui-pjt/src/views/homepage/Homepage.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="home-wrap">
-      123
+      <section class="home-header">
+          <h3>管理后台</h3>
+          <i class="el-icon-menu"></i>
+            <el-dropdown class="header-dropdown">
+                <span class="el-dropdown-link">
+                    下拉菜单<i class="el-icon-arrow-down el-icon--right"></i>
+                </span>
+                <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>退出登录</el-dropdown-item>
+                </el-dropdown-menu>
+            </el-dropdown>
+      </section>
+      <section class="home-menu">
+
+      </section>
+      <section class="home-content">
+
+      </section>
   </div>
 </template>
 
@@ -10,6 +27,32 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang="less">
+.home-wrap{
+    height:100%;
 
+    .home-header{
+        height:70px;
+        line-height:70px;
+        background:#409eff;
+
+        h3{
+            display:inline-block;
+            color:#fff;
+            margin:0 10px;
+            font-size:24px;
+        }
+        .el-icon-menu{
+            color:#fff;
+            font-size:20px;
+        }
+
+        .header-dropdown{
+            float:right;
+            .el-dropdown-link,.el-icon-arrow-down{
+                color:#fff;
+            }
+        }
+    }
+}
 </style>
