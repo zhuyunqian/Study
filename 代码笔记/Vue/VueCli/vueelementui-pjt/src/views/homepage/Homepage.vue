b7c46cea8dc098d60be7169267582ba78a8535c0
zhutongxue <qtzhuyunqian@163.com>
2020-04-18 16:57:23 +0800

    Vue -- elementUi -- 后台管理项目 -- home页面 -- 侧边栏（导航栏组件）

--- 代码笔记/Vue/VueCli/vueelementui-pjt/src/views/homepage/Homepage.vue
+++ 代码笔记/Vue/VueCli/vueelementui-pjt/src/views/homepage/Homepage.vue
@@ -12,9 +12,27 @@
                 </el-dropdown-menu>
             </el-dropdown>
       </section>
-      <section class="home-menu">
-
-      </section>
+      <!-- <section class="home-menu"> -->
+        <el-menu
+                default-active="4"
+                class="home-menu"
+                background-color="#324157"
+                text-color="#fff"
+                active-text-color="#20a0ff">
+                <el-submenu index="1">
+                    <template slot="title">
+                    <i class="el-icon-location"></i>
+                    <span>系统管理</span>
+                    </template>
+                    <el-menu-item index="1-1">部门管理</el-menu-item>
+                    <el-menu-item index="1-2">员工管理</el-menu-item>
+                    <el-menu-item index="1-3">权限管理</el-menu-item>
+                    <el-menu-item index="1-4">角色管理</el-menu-item>
+                
+                </el-submenu>
+                
+        </el-menu>
+      <!-- </section> -->
       <section class="home-content">
 
       </section>
@@ -53,6 +71,23 @@ export default {
                 color:#fff;
             }
         }
+    }
+
+    .home-menu{
+        position: absolute;
+        left:0;
+        top:70px;
+        width:250px;
+        bottom:0;
+        // 这里可以让占满屏幕
+    }
+    .home-content{
+        position:absolute;
+        left:250px;
+        top:70px;
+        right:0;
+        bottom:0;
+        background:skyblue;
     }
 }
 </style>
