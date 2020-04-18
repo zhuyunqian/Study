bfa0c3396d7d6b05ba3bcd0dae4715850f49d647
zhutongxue <qtzhuyunqian@163.com>
2020-04-18 18:34:29 +0800

    Vue -- elementUi -- 后台管理项目 -- home页面 -- 侧边栏（配置选中显示高亮）

--- 代码笔记/Vue/VueCli/vueelementui-pjt/src/views/homepage/children/Menu.vue
+++ 代码笔记/Vue/VueCli/vueelementui-pjt/src/views/homepage/children/Menu.vue
@@ -1,21 +1,30 @@
 <template>
    <!-- <section class="home-menu"> -->
+       <!-- 
+           改变选中状态，获取当前路由的路径赋值
+           this.$route.path
+           unique-opened -- 默认只打开一个折叠菜单
+        -->
         <el-menu
-                default-active="4"
+                :default-active="activeRoute"
                 class="home-menu"
                 background-color="#324157"
                 text-color="#fff"
                 active-text-color="#20a0ff"
-                :router="true">
-                <el-submenu index="1">
+                :router="true"
+                unique-opened="true"
+                >
+                
+                <!-- v-for循环 -->
+                <el-submenu v-for="(menu , index) in menuConfig" :key="index" :index="index+''" >
                     <template slot="title">
-                    <i class="el-icon-location"></i>
-                    <span>系统管理</span>
+                    <i :class="menu.icon"></i>
+                    <span>{{menu.title}}</span>
                     </template>
-                    <el-menu-item index="/department">部门管理</el-menu-item>
-                    <el-menu-item index="1-2">员工管理</el-menu-item>
-                    <el-menu-item index="1-3">权限管理</el-menu-item>
-                    <el-menu-item index="1-4">角色管理</el-menu-item>
+                    <el-menu-item v-for="(menusub , index) in menu.subs" :key="index" :index="menusub.index">
+                        {{menusub.title}}
+                    </el-menu-item>
+                    
                 
                 </el-submenu>
                 
@@ -24,8 +33,21 @@
 </template>
 
 <script>
-export default {
+// 引入需要遍历的js数据文件
+import { menuConfig } from '../../../config/menuconfig'
 
+export default {
+    data() {
+        return {
+            // 简式写法， 这里的属性 == 引入的js的变量
+            menuConfig
+        }
+    },
+    computed: {
+        activeRoute(){
+            return this.$route.path
+        }
+    },
 }
 </script>
 
