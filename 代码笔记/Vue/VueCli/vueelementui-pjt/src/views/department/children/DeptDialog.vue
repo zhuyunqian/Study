31e260ab336611914181c9d90db2ff60e6b6cded
zhutongxue <qtzhuyunqian@163.com>
2020-04-20 15:27:45 +0800

    Vue -- elementUi -- 后台管理项目 -- home页面 -- 部门管理 -- 组件页面与数据值同步问题解决（watch对象）

--- 代码笔记/Vue/VueCli/vueelementui-pjt/src/views/department/children/DeptDialog.vue
+++ 代码笔记/Vue/VueCli/vueelementui-pjt/src/views/department/children/DeptDialog.vue
@@ -5,12 +5,12 @@
         根据组件的属性beforeclose书写 关闭前回调函数
      -->
       <el-dialog title="编辑部门" :visible.sync="dialog" :before-close="close">
-        <el-form :model="diaDate">
+        <el-form :model="form">
           <el-form-item label="部门名称" :label-width="formLabelWidth">
-            <el-input v-model="diaDate.name" autocomplete="off"></el-input>
+            <el-input v-model="form.name" autocomplete="off"></el-input>
           </el-form-item>
           <el-form-item label="部门编号" :label-width="formLabelWidth">
-            <el-input v-model="diaDate.sn" autocomplete="off"></el-input>
+            <el-input v-model="form.sn" autocomplete="off"></el-input>
           </el-form-item>
 
         </el-form>
@@ -47,6 +47,17 @@ export default {
       // dialogFormVisible:false,
       form:{},
       formLabelWidth:'100px'
+    }
+  },
+  /*
+    由于v-model 是 数据层和 view层同步  --  所以传输过来的data直接进行了改变 这里需要见天数据发生改变再进行改变
+    watch -- view监听对象
+  */
+  watch: {
+    diaDate(val,oldval){
+      console.log(val)
+      console.log(oldval)
+      this.form = JSON.parse(JSON.stringify(val));
     }
   },
   methods: {
