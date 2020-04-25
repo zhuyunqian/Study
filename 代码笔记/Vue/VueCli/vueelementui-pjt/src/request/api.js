bd21d5434d681dae0000994b216f2836bb45e69a
zhutongxue <qtzhuyunqian@163.com>
2020-04-25 11:09:58 +0800

    Vue -- elementUi -- 后台管理项目 -- 封装网络请求 -- 环境判断，设置代理

--- 代码笔记/Vue/VueCli/vueelementui-pjt/src/request/api.js
+++ 代码笔记/Vue/VueCli/vueelementui-pjt/src/request/api.js
@@ -4,31 +4,36 @@
 import request from './request'
 
 
+// 上线路由判断，小封装
+//const path = '/api'
+//如果是生产环境为空，否则（开发环境）为 /api代理
+const path = process.env.NODE_ENV === 'production' ? '' : '/api'
+
 //登陆
-export const apiLogin = data => request.post("/api/tokens", data);
+export const apiLogin = data => request.post(path + "/tokens", data);
 
 //获取部门列表
-export const getDepartment = data => request.get("/api/departments", {params: data});
+export const getDepartment = data => request.get(path + "/departments", {params: data});
 
 //添加部门
 //注意这里的传输get要求一定要根据要求，不然可能不会报错，但是不实现
-export const addDepartment = data => request.post("/api/departments", data);
+export const addDepartment = data => request.post(path + "/departments", data);
 
 //编辑部门
-export const getDepartmentId = data => request.put('/api/departments/'+ data.id, data)
+export const getDepartmentId = data => request.put( path + '/departments/'+ data.id, data)
 
 //员工管理部门
-export const getEmployees = data => request.get('/api/employees',{params:data});
+export const getEmployees = data => request.get( path + '/employees',{params:data});
 
 //查看单个员工请求
-export const getEditemployees = data => request.get('/api/employees/'+data.id, data);
+export const getEditemployees = data => request.get( path + '/employees/'+data.id, data);
 
 //角色列表
-export const getRoles = data => request.get('/api/roles',{params:data});
+export const getRoles = data => request.get( path + '/roles',{params:data});
 
 //保存单个员工信息
-export const saveEmployee = data => request.put('/api/employees/'+ data.id, data)
+export const saveEmployee = data => request.put( path + '/employees/'+ data.id, data)
 
 //导出员工列表
-export const eportEmployee =  '/api/employees/xlses'
+export const eportEmployee =   path + '/employees/xlses'
 
