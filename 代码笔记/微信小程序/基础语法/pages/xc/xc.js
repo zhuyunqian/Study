158f70e6ec2c2a297dc1907a1689a2e887202024
zhutongxue <qtzhuyunqian@163.com>
2020-04-26 17:05:39 +0800

    微信小程序 -- 携程 -- wx:for={{}}循环遍历

--- 代码笔记/微信小程序/基础语法/pages/xc/xc.js
+++ 代码笔记/微信小程序/基础语法/pages/xc/xc.js
@@ -5,7 +5,50 @@ Page({
    * 页面的初始数据
    */
   data: {
+    itemBeans: [
+      {
+        "type": 0,
+        "imageUrl": "../assets/images/b_hotel.png",
+        "title": "酒店",
+        "tagUrl": "",
+        "items": [
+          { "imageUrl": "", "title": "海外酒店", "tagUrl": "" },
+          { "imageUrl": "", "title": "团购", "tagUrl": "" },
+          { "imageUrl": "", "title": "特价酒店", "tagUrl": "" },
+          { "imageUrl": "", "title": "民宿酒店", "tagUrl": "" }
+        ],
+        "bgColor": "#FB5265"
+      },
 
+      {
+        "type": 0,
+        "imageUrl": "../assets/images/plane.png",
+        "title": "机票",
+        "tagUrl": "",
+        "items": [
+          { "imageUrl": "", "title": "火车盘票", "tagUrl": "http://images4.c-ctrip.com/target/zc010i0000009iwizFEDF.png_.webp" },
+          { "imageUrl": "", "title": "汽车票.船票", "tagUrl": "" },
+          { "imageUrl": "", "title": "特价机票", "tagUrl": "" },
+          { "imageUrl": "", "title": "专车.租车", "tagUrl": "" }
+        ],
+        "bgColor": "#3C99FF"
+      },
+
+      {
+        "type": 0,
+        "imageUrl": "../assets/images/goodess.png",
+        "title": "旅游",
+        "tagUrl": "",
+        "items": [
+          { "imageUrl": "", "title": "目的地攻略", "tagUrl": "" },
+          { "imageUrl": "", "title": "游轮旅游", "tagUrl": "" },
+          { "imageUrl": "", "title": "周边游", "tagUrl": "" },
+          { "imageUrl": "", "title": "定制旅行", "tagUrl": "" }
+        ],
+        "bgColor": "#50C72D"
+      },
+
+    ],
   },
 
   /**
