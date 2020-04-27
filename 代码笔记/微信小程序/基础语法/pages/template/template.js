74489e009588e96099edc670420a306f68632b74
zhutongxue <qtzhuyunqian@163.com>
2020-04-27 11:49:00 +0800

    微信小程序 -- 基础语法 -- 模版引入（template）

--- 代码笔记/微信小程序/基础语法/pages/template/template.js
+++ 代码笔记/微信小程序/基础语法/pages/template/template.js
@@ -5,7 +5,29 @@ Page({
    * 页面的初始数据
    */
   data: {
-    bool:true
+    bool:true,
+    json:[
+      {	"imgUrl":"https://img14.360buyimg.com/n1/s150x150_jfs/t30814/9/944457966/151730/b9082df7/5c01fcbbNa3869e29.jpg.dpg",
+     "price":"1699",
+     "outPrice":"1899"  
+      },
+      {	"imgUrl":"https://img14.360buyimg.com/n1/s150x150_jfs/t30409/283/889241385/203388/d44ae817/5c00d21aNcfbb2aa7.jpg.dpg",
+     "price":"38.7",
+     "outPrice":"60"  
+      },
+      {	"imgUrl":"https://img14.360buyimg.com/n1/s150x150_jfs/t1/17561/2/174/181822/5c078d55E6b3dd312/001c59776d959381.jpg.dpg",
+     "price":"1899",
+     "outPrice":"2899"  
+      },
+      {	"imgUrl":"https://img14.360buyimg.com/n1/s150x150_jfs/t26470/12/2593089004/174952/e153816b/5c0633c1N80aff116.jpg.dpg",
+     "price":"28.5",
+     "outPrice":"35.9"  
+      },
+      {	"imgUrl":"https://img14.360buyimg.com/n1/s150x150_jfs/t1411/245/746207504/132419/981fb579/55a8d386N2cbd7ba1.jpg.dpg",
+     "price":"5399",
+     "outPrice":"6799"  
+      }
+    ]
   },
   toggle(e){
     this.setData({
--- 代码笔记/微信小程序/基础语法/pages/text/text.js
+++ 代码笔记/微信小程序/基础语法/pages/text/text.js
@@ -8,27 +8,72 @@ Page({
     text:'11112313342324',
     bool:false,
     imgSrc:'',
+    lsit1:[
+      {	"imgUrl":"https://img14.360buyimg.com/n1/s150x150_jfs/t30409/283/889241385/203388/d44ae817/5c00d21aNcfbb2aa7.jpg.dpg",
+      "price":"38.7",
+      "outPrice":"60"  
+       }, 
+      {	"imgUrl":"https://img14.360buyimg.com/n1/s150x150_jfs/t30814/9/944457966/151730/b9082df7/5c01fcbbNa3869e29.jpg.dpg",
+     "price":"1699",
+     "outPrice":"1899"  
+      },
+      {	"imgUrl":"https://img14.360buyimg.com/n1/s150x150_jfs/t1/17561/2/174/181822/5c078d55E6b3dd312/001c59776d959381.jpg.dpg",
+     "price":"1899",
+     "outPrice":"2899"  
+      },
+      {	"imgUrl":"https://img14.360buyimg.com/n1/s150x150_jfs/t26470/12/2593089004/174952/e153816b/5c0633c1N80aff116.jpg.dpg",
+     "price":"28.5",
+     "outPrice":"35.9"  
+      },
+      {	"imgUrl":"https://img14.360buyimg.com/n1/s150x150_jfs/t1411/245/746207504/132419/981fb579/55a8d386N2cbd7ba1.jpg.dpg",
+     "price":"5399",
+     "outPrice":"6799"  
+      }
+    ],
+    lsit3:[
+      {	"imgUrl":"https://img14.360buyimg.com/n1/s150x150_jfs/t26470/12/2593089004/174952/e153816b/5c0633c1N80aff116.jpg.dpg",
+     "price":"28.5",
+     "outPrice":"35.9"  
+      },
+      {	"imgUrl":"https://img14.360buyimg.com/n1/s150x150_jfs/t1411/245/746207504/132419/981fb579/55a8d386N2cbd7ba1.jpg.dpg",
+     "price":"5399",
+     "outPrice":"6799"  
+      },
+      {	"imgUrl":"https://img14.360buyimg.com/n1/s150x150_jfs/t30409/283/889241385/203388/d44ae817/5c00d21aNcfbb2aa7.jpg.dpg",
+      "price":"38.7",
+      "outPrice":"60"  
+       }, 
+      {	"imgUrl":"https://img14.360buyimg.com/n1/s150x150_jfs/t30814/9/944457966/151730/b9082df7/5c01fcbbNa3869e29.jpg.dpg",
+     "price":"1699",
+     "outPrice":"1899"  
+      },
+      {	"imgUrl":"https://img14.360buyimg.com/n1/s150x150_jfs/t1/17561/2/174/181822/5c078d55E6b3dd312/001c59776d959381.jpg.dpg",
+     "price":"1899",
+     "outPrice":"2899"  
+      },
+
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var _this = this
-    wx.chooseImage({
-      count: 1,
-      sizeType: ['original', 'compressed'],
-      sourceType: ['album', 'camera'],
-      success (res) {
-        // tempFilePath可以作为img标签的src属性显示图片
-        // const tempFilePaths = res.tempFilePaths
-        // 双向数据绑定，js和界面数据都更新
-        console.log(res)
-        _this.setData({
-          imgSrc:res.tempFilePaths[0]
-        })
-      }
-    })
+    // var _this = this
+    // wx.chooseImage({
+    //   count: 1,
+    //   sizeType: ['original', 'compressed'],
+    //   sourceType: ['album', 'camera'],
+    //   success (res) {
+    //     // tempFilePath可以作为img标签的src属性显示图片
+    //     // const tempFilePaths = res.tempFilePaths
+    //     // 双向数据绑定，js和界面数据都更新
+    //     console.log(res)
+    //     _this.setData({
+    //       imgSrc:res.tempFilePaths[0]
+    //     })
+    //   }
+    // })
   },
 
   /**
