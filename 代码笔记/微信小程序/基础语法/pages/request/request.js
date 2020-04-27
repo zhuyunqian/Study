ac61902e0dbfc03502432c68347d17cec1ae4c24
zhutongxue <qtzhuyunqian@163.com>
2020-04-27 20:52:03 +0800

    微信小程序 -- 基础语法 -- wx.request - 请求

---
+++ 代码笔记/微信小程序/基础语法/pages/request/request.js
@@ -0,0 +1,86 @@
+// pages/request/request.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    username:'terry',
+    password:1
+  },
+
+  req(e){
+    wx.request({
+      url: 'http://kumanxuan1.f3322.net:8084/tokens',
+      data: {
+        username:this.data.username,
+        password:this.data.password
+      },
+      header: {'content-type':'application/json'},
+      method: 'GET',
+      dataType: 'json',
+      responseType: 'text',
+      success: (result)=>{
+        console.log(result)
+      },
+      fail: ()=>{},
+      complete: ()=>{}
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
