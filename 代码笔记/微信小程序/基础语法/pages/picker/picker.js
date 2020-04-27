7ca6700fafa7548fd0c08c5d150921b9dc7e1cd8
zhutongxue <qtzhuyunqian@163.com>
2020-04-27 15:32:58 +0800

    微信小程序 -- 基础语法 -- picker -- 普通选择器

---
+++ 代码笔记/微信小程序/基础语法/pages/picker/picker.js
@@ -0,0 +1,78 @@
+// pages/picker/picker.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    array: ['美国', '中国', '巴西', '日本'],
+    index:0
+  },
+  // 改变事件
+  change(e){
+    console.log(e)
+    this.setData({
+      // e.detail.value 改变后的索引值
+      index:e.detail.value
+    })
+  },
+  //取消事件
+  cancel(e){
+    console.log(e)
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
