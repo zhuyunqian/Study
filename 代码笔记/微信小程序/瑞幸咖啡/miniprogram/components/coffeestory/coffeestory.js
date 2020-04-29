28bb31e6946134d7969ce0a952083fdaf4fb61d7
zhutongxue <qtzhuyunqian@163.com>
2020-04-30 00:33:30 +0800

    微信小程序 -- 云开发 -- 瑞幸咖啡 -- scrollview组件化（flex-shank）

---
+++ 代码笔记/微信小程序/瑞幸咖啡/miniprogram/components/coffeestory/coffeestory.js
@@ -0,0 +1,56 @@
+// components/coffeestory/coffeestory.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    boxlists: {
+      items0: {
+        src: "/images/home/card01.jpg",
+        text: '给特别的你'
+      },
+      items1: {
+        src: "/images/home/card02.jpg",
+        text: '我想对你说'
+      },
+      items2: {
+        src: "/images/home/card03.jpg",
+        text: '勇敢爱'
+      },
+      items3: {
+        src: "/images/home/card04.jpg",
+        text: '一个人的精彩'
+      },
+      items4: {
+        src: "/images/home/card05.jpg",
+        text: '萌萌哒'
+      },
+      items5: {
+        src: "/images/home/card06.jpg",
+        text: '友谊万岁'
+      },
+      items6: {
+        src: "/images/home/card07.jpg",
+        text: '恋人未满'
+      },
+      items7: {
+        src: "/images/home/card08.jpg",
+        text: '生日快乐'
+      }
+    },
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
