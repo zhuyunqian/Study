e1e9fef00103861b8a320b057564ad4de72a8122
zhutongxue <qtzhuyunqian@163.com>
2020-04-29 16:20:53 +0800

    微信小程序 -- 云开发 -- 瑞幸咖啡 -- 轮播组件引入

---
+++ 代码笔记/微信小程序/瑞幸咖啡/miniprogram/components/myswiper/myswiper.js
@@ -0,0 +1,28 @@
+// components/myswiper/myswiper.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    banners: [
+      "/images/banner/baner1.jpg",
+      "/images/banner/baner2.jpg",
+      "/images/banner/baner3.jpg",
+      "/images/banner/baner4.jpg",
+    ],
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
