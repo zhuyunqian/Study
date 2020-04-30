25da26cfe60ca13d93429bf3d6a8a00915a3ace7
zhutongxue <qtzhuyunqian@163.com>
2020-04-30 19:09:03 +0800

    Vue -- elementUi -- v-charts - 折线图和K线图

--- 代码笔记/Vue/VueCli/vueelementui-pjt/src/plugins/v-chart.js
+++ 代码笔记/Vue/VueCli/vueelementui-pjt/src/plugins/v-chart.js
@@ -1,4 +1,6 @@
 import Vue from 'vue';
 import VeLine from 'v-charts/lib/line.common'
+import VeCandle from 'v-charts/lib/candle.common'
 
 Vue.component(VeLine.name, VeLine);
+Vue.component(VeCandle.name, VeCandle);
