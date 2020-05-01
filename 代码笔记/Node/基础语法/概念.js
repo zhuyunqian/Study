f2bc7b2793e9fb717bc2e6df5ca53d8baee5b081
zhutongxue <qtzhuyunqian@163.com>
2020-05-01 10:51:50 +0800

    NodeJs -- 基础语法  --  nvm使用

--- 代码笔记/Node/基础语法/概念.js
+++ 代码笔记/Node/基础语法/概念.js
@@ -11,6 +11,13 @@ node -- 编译解释js代码
 终端 node -- 进入交互模式 ，为了快速验证 - 正常不会用于工作模式
 
 
+- nvm version：           nvm的版本
+- nvm list：              安装所有的版本
+- nvm install 版本号 [架构]：    安装指定版本
+- nvm uninstall 版本号：  卸载指定版本
+- nvm use 版本号：        选择使用指定版本
 
 
 */
+
+console.log('123');
