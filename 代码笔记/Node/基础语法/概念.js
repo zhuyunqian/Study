081c65b5b0a3bfe85ff9587dc9f190f15f495706
zhutongxue <qtzhuyunqian@163.com>
2020-05-01 10:33:33 +0800

    NodeJs  --  概念

---
+++ 代码笔记/Node/基础语法/概念.js
@@ -0,0 +1,16 @@
+/*
+
+Node.js®是基于 Chrome的V8 JavaScript 引擎构建的JavaScript运行环境。
+
+Node.js不是新语言，也不是一个框架或者一个库，而是一个软件。
+
+Node.js是一个 JavaScript 运行环境 ，说白了，就是用来运行js的。
+
+node -- 编译解释js代码
+
+终端 node -- 进入交互模式 ，为了快速验证 - 正常不会用于工作模式
+
+
+
+
+*/
