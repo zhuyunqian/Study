ddc394b8b30d3380e0fcfaa47edc92a80ff4d22c
zhutongxue <qtzhuyunqian@163.com>
2020-05-02 00:36:57 +0800

    Uniapp -- 链家 -- 首页 -- 头图

--- 代码笔记/Uniapp/LinkHome/pages/home/home.vue
+++ 代码笔记/Uniapp/LinkHome/pages/home/home.vue
@@ -1,6 +1,18 @@
 <template>
 	<view>
-		
+		<view class="banner">
+			<image src="../../static/images/banner.jpg"></image>
+			<view class="search">
+				<view class="searchLeft">
+					广州
+				</view>
+				<view class="searchRight">
+					<text class="iconfont">&#xe692;</text>
+					<input type="text" placeholder="输入您想住哪里？">
+				</view>
+			</view>
+		</view>
+
 	</view>
 </template>
 
@@ -8,12 +20,70 @@
 	export default {
 		data() {
 			return {
-				
+
 			};
 		}
 	}
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+	.banner {
+		width: 100%;
+		height: 400rpx;
+		position: relative;
 
+		image {
+			width: 100%;
+			height: 400rpx;
+			position: absolute;
+		}
+
+		.search {
+			position: absolute;
+			z-index: 1;
+			top: 100rpx;
+			width: 90%;
+			left: 5%;
+			height: 88rpx;
+			background-color: #fff;
+			box-sizing: border-box;
+			
+			.searchLeft{
+				margin-left:20rpx;
+				font-size:28rpx;
+				line-height:88rpx;
+				float:left;
+				&::after{
+					content:"";
+					width:0;
+					height:0;
+					display:inline-block;
+					border:10rpx solid transparent;
+					border-top-color:#ababab;
+					position:relative;
+					left:8rpx;
+					top:4rpx;
+				}
+			}
+			.searchRight{
+				float:right;
+				width:80%;
+				height:88rpx;
+				
+				text.iconfont{
+					line-height:88rpx;
+					display:inline-block;
+					float:left;
+				}
+				
+				input{
+					float:right;
+					width:90%;
+					height:88rpx;
+					line-height:88rpx;
+					font-size:28rpx;
+				}
+			}
+		}
+	}
 </style>
