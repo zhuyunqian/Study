32aeb45eb89cc7e7985a3c5f175cf4480acbacf3
zhutongxue <qtzhuyunqian@163.com>
2020-05-03 00:53:04 +0800

    NodeJs -- Es6 -- 基础语法 -- 面向对象  --  类的定义与实例

--- 代码笔记/Node/基础语法/面向对象.js
+++ 代码笔记/Node/基础语法/面向对象.js
@@ -24,3 +24,43 @@ a.showName();
 // 调用类的静态方法
 Animal.eat();
 
+
+
+// es6方法
+/*
+
+1 - 实例属性和实例方法都是给实例对象去调用的
+2 - 每一个实例对象在内存中是独立的，拥有自己的属性和方法和内存，互不干扰
+3 - 实例一个对象并传参  --  创建了一个新对象  - 调用constructor方法，并传入参数  --  对象指向给dog标识符
+4 - 静态方法通过类名调用
+5 - 静态属性定义 -- 类外面定义  --  类名.属性名 = 值
+
+*/
+class Animal{
+
+    // 定义构造函数
+    constructor(name){
+        // - 实力化对象 - 即调用 - 属性在这里传参并定义
+        // console.log("constructor");
+        this.name = name;
+    }
+
+    // 定义一个方法 - 实例方法
+    showName(){
+        console.log(this.name);
+    }
+
+    //定义静态方法
+    static eat(){
+        console.log("吃---");
+    }
+}
+
+class Cat extends Animal{
+
+}
+
+let cat1 = new Cat("Tom");
+
+cat1.showName();
+Cat.eat();
