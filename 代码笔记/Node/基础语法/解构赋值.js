6f2db6d64dbc6c289b8c0cbd012cd6c1f89f8d6f
zhutongxue <qtzhuyunqian@163.com>
2020-05-02 18:51:08 +0800

    NodeJs -- Es6 -- 基础语法 -- 解构赋值函数  --  参数默认值指定

--- 代码笔记/Node/基础语法/解构赋值.js
+++ 代码笔记/Node/基础语法/解构赋值.js
@@ -19,3 +19,17 @@ arr(array);
 // 注意：
 //func(); //相当于传了一个null   {name, age}=null 就会报错
 func({});  //不会报错，输出：undefined undefined
+
+
+// 解构赋值制定参数的默认值
+
+function func2({name, age} = {}){   //防止不传实参时候的报错  -- 不传参数的时候会报错没有={}
+    console.log(name, age);
+}
+func2();   //undefined undefined  
+
+// 指定默认参数
+function func3({name="nodejs", age=11} = {}){    //指定默认值
+    console.log(name, age);
+}
+func3();  //nodejs 11
