8b631af22eac4d42721278c80fcb0cba8745db8d
zhutongxue <qtzhuyunqian@163.com>
2020-05-10 12:44:30 +0800

    NodeJs -- 基础语法 -- 内置模块 -- fs -- 同步readFileSync(). 异步readFille(filepath,'utf-8',(error,data)=>{})

---
+++ 代码笔记/Node/内置模块/fs.js
@@ -0,0 +1,35 @@
+/*
+fs模块 -- 作用文件操作
+
+同步读取 -- readFileSync -- 文件读取完毕，才会执行后面的代码 -- 排队机制，同步阻塞
+读取出计算机认识的buffer格式 
+-- toString() 转化为字符串
+-- fs.readFileSync(filePath,'utf-8')  ，注明要求utf-8格式
+
+
+异步读取 -- readFile -- 文件读取完毕，有个回调函数执行
+fs.readFile(filePath,'utf-8',(error,data)=>{})
+error -- 读取成功为null 否则为object
+data -- 读取成功为读取信息data 否则为undefined
+*/
+
+// 同步读取 -- readFileSync
+const fs = require('fs');
+const path = require('path');
+let filePath = path.join(__dirname,'readfs.js')
+
+// let readmes = fs.readFileSync(filePath,'utf-8')
+// console.log(readmes)
+// console.log('----------')
+// console.log(readmes.toString())
+
+
+// 异步读取 -- readFile
+fs.readFile(filePath,'utf-8',(error,data)=>{
+    // 重点回调函数的执行
+    if(error){
+        console.log(error)
+        return;
+    }
+    console.log(data)
+})
---
+++ 代码笔记/Node/内置模块/readfs.js
@@ -0,0 +1,2 @@
+nodeJs 
+const node = nodeJs
