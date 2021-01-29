cac06809c7186e544eb8599eb2a487142f41fc5f
zyq <zyq>
2021-01-29 17:25:40 +0800

    Promise-- all方法

--- 代码笔记/Node/promise.js
+++ 代码笔记/Node/promise.js
@@ -5,6 +5,7 @@ const util = require("util")
 let filePath1 = path.join(__dirname, "testPro", "1.txt");
 let filePath2 = path.join(__dirname, "testPro", "2.txt");
 let filePath3 = path.join(__dirname, "testPro", "3.txt");
+
 // util.promisify -- 返回一个promise对象 有限error优先，对象 （error,data）=>{}
 let readFilePromise = util.promisify(fs.readFile)
 
@@ -24,19 +25,33 @@ let readFilePromise = util.promisify(fs.readFile)
 
 let str1 = "";
 
-readFilePromise(filePath1).then((data)=>{
-    // 成功执行
-    str1+=data;
-    return readFilePromise(filePath2)
+let p1 = readFilePromise(filePath1,'utf-8')
+let p2 = readFilePromise(filePath2,'utf-8')
+let p3 = readFilePromise(filePath3,'utf-8')
+
+// 多个promise对象同时执行
+Promise.all([p1,p2,p3]).then((data)=>{
+    console.log(data)
 }).catch((error)=>{
-    //异步失败执行
+    //一旦又一个报错，执行这里的方法
     console.log(error)
 }).finally(()=>{
-    console.log('不管错误正确，都会执行')
-}).then((data)=>{
-    str1+=data;
-    return readFilePromise(filePath3);
-}).then((data)=>{
-    str1+=data;
-    console.log(str1);
-});
+    console.log(0)
+})
+
+// readFilePromise(filePath1).then((data)=>{
+//     // 成功执行
+//     str1+=data;
+//     return readFilePromise(filePath2)
+// }).catch((error)=>{
+//     //异步失败执行
+//     console.log(error)
+// }).finally(()=>{
+//     console.log('不管错误正确，都会执行')
+// }).then((data)=>{
+//     str1+=data;
+//     return readFilePromise(filePath3);
+// }).then((data)=>{
+//     str1+=data;
+//     console.log(str1);
+// });
