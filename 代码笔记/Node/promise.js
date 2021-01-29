a5e6a7e6ff4ac158b983be86096872de097bf8f5
lvheguang <qtzhuyunqian@163.com>
2021-01-29 14:49:05 +0800

    Node -- Api -- util.promisify处理异步

--- 代码笔记/Node/promise.js
+++ 代码笔记/Node/promise.js
@@ -1,23 +1,26 @@
 const fs = require("fs");
 const path = require("path");
+const util = require("util")
 
 let filePath1 = path.join(__dirname, "testPro", "1.txt");
 let filePath2 = path.join(__dirname, "testPro", "2.txt");
 let filePath3 = path.join(__dirname, "testPro", "3.txt");
+// util.promisify -- 返回一个promise对象 有限error优先，对象 （error,data）=>{}
+let readFilePromise = util.promisify(fs.readFile)
 
-function readFilePromise(filePath){
-    return new Promise((resolve, reject)=>{
+// function readFilePromise(filePath){
+//     return new Promise((resolve, reject)=>{
 
-        fs.readFile(filePath,"utf-8",(error1, data1)=>{
-            if(error1){
-                //失败的时候做的事情
-                reject(error1);
-            }
-            //请求成功 -- resolve
-            resolve(data1)
-        })
-    });
-}
+//         fs.readFile(filePath,"utf-8",(error1, data1)=>{
+//             if(error1){
+//                 //失败的时候做的事情
+//                 reject(error1);
+//             }
+//             //请求成功 -- resolve
+//             resolve(data1)
+//         })
+//     });
+// }
 
 let str1 = "";
 
