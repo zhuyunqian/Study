ee4a36218d80d51e6e0b76df20c53acec2377c85
zyq <zyq>
2021-02-01 17:53:37 +0800

    异步处理 -- anysc -- 错误捕捉处理（内部、外部）

--- 代码笔记/Node/promise.js
+++ 代码笔记/Node/promise.js
@@ -30,14 +30,31 @@ let str1 = "";
 // let p3 = readFilePromise(filePath3,'utf-8')
 
 async function func(){
-    let data1 = await readFile(filePath1, "utf-8");  // 1. await（跟promise对象） 可直接获取数据(即获得reslove返回的值)  必须基于async写法
-    let data2 = await readFile(filePath2, "utf-8");  // 2. await为异步 ， 异步同步执行 异步1，异步2，异步3
-    let data3 = await readFile(filePath3, "utf-8");  // 3. await后跟一个基本数据类型，会对这个基本数据类型进行包装，成为一个promise对象
-                                                     // await 123 ==== new Promise((resolve,reject)=>{resolve(123)})
-    console.log(data1 +data2 +data3)
+
+    //内部错误捕捉
+    try{
+        let data1 = await readFilePromise(filePath1, "utf-8");  // 1. await（跟promise对象） 可直接获取数据(即获得reslove返回的值)  必须基于async写法
+        let data2 = await readFilePromise(filePath2, "utf-8");  // 2. await为异步 ， 异步同步执行 异步1，异步2，异步3
+        let data3 = await readFilePromise(filePath3, "utf-8");  // 3. await后跟一个基本数据类型，会对这个基本数据类型进行包装，成为一个promise对象
+                                                         // await 123 ==== new Promise((resolve,reject)=>{resolve(123)})
+        console.log(data1 +data2 +data3)
+    
+    }
+    catch(error){
+        console.log(error)
+    }
+    finally{
+        console.log("finally")
+    }
+
 }
 
-func();
+//外部错误捕捉
+func().catch((error)=>{
+    console.log(error)
+}).finally(()=>{
+    console.log('finally')
+})
 
 // 多个promise对象同时执行
 // Promise.all([p1,p2,p3]).then((data)=>{
