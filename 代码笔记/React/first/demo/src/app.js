244a58b3d584dc66a28e52639bd8c5a6a96b7f13
zyq <zyq>
2021-02-05 18:25:39 +0800

    React -- 点击事件onclick

--- 代码笔记/React/first/demo/src/app.js
+++ 代码笔记/React/first/demo/src/app.js
@@ -5,9 +5,33 @@ import ReactDOM from 'react-dom'
 // 定义组件
 
 class App extends React.Component{
+    //构造函数 -- 对象继承走的函数 ， 定义state
+    constructor(props){
+        super(props);
+        
+        this.state={
+            num:20
+        }
+    }
+
+    // 点击事件，里面的this没有指向，需要在onclick的时候绑定this
+    clickNum(){
+        this.setState({
+            num : this.state.num+1
+        })
+    }
+
     render(){
+        
         return(
-            <div>组件化开发</div>
+            <div>
+                组件化开发
+
+                <div>{this.state.num}</div><br/>
+                {/* 这里绑定bind(this) */}
+                <button onClick={this.clickNum.bind(this)}>点击增加</button>
+
+            </div>
         )
     }
 }
