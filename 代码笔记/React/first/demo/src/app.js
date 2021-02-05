592ca41cb11b027ab4f88041e347737325c1bf0c
zyq <zyq>
2021-02-05 17:40:57 +0800

    React -- 组件化开发 -- 语法

---
+++ 代码笔记/React/first/demo/src/app.js
@@ -0,0 +1,16 @@
+// 引入核心模块
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// 定义组件
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>组件化开发</div>
+        )
+    }
+}
+
+// 导出组件
+export default App
--- 代码笔记/React/first/demo/src/index.js
+++ 代码笔记/React/first/demo/src/index.js
@@ -3,11 +3,12 @@
 // 引入核心模块
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './app.js'
 
 // 通过jsx语法将组件、标签渲染到标签上
 ReactDOM.render(
     //渲染上去的内容（参数1）
-    <div>hello</div>,
+    <App/>,
     //渲染的标签（参数2）
     document.getElementById('root')
 )
