63bed6b773815b2a509511916492c3524481a57d
zyq <zyq>
2021-02-22 15:24:46 +0800

    React -- 定义props数据类型 -- 1.yarn add prop-types 2.引入 3.使用

--- 代码笔记/React/first/demo/src/app2.js
+++ 代码笔记/React/first/demo/src/app2.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react'
+import PropsTypes from 'prop-types'
 import "./assets/index.css"
 
 // 定义组件
 class Header extends Component {
     // 定义默认未父子传参属性
     static defaultProps = {
-        title: '默认标题'
+        title: PropsTypes.string
     }
     render(){
         return (
-            <header style={{'background':'pink'}}>{this.props.title}</header>
+            <div>
+                <header style={{'background':'pink'}}>{this.props.title}</header>
+            </div>
         )
     }
 } 
